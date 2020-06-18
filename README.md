84c128c0356f1ce4d9e1e5a96f16e6029d4b70d6
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-06-17 22:45:54 -0300

    all tests passed

---
+++ README.md
@@ -0,0 +1,30 @@
+<img alt="GoStack" src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/header-desafios.png" />
+
+# GoStack Rocketseat Challenge 1
+This project is the 2nd challenge of GoStack Bootcamp by Rocketseat
+
+Our goal was to create a Front-end application to add or remove repositories from the Back-end developed on last challenge
+
+---
+
+In order to find out more, check out the **[instructions repository](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-reactjs)**
+
+This challenge was tested using and it should follow the following instructions:
+- [x] list API repositories
+- [x] add repository to the API
+- [x] remove repository from API
+
+---
+
+### Usage
+If you would like to explore this project, feel free to do so
+To get it running just install all the dependencies by typing 
+
+```bash
+yarn install
+```
+on your terminal
+
+---
+
+🚀
--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import api from './services/api'
+import './styles.css'
 
-import "./styles.css";
+function App () {
+  // defining empty repository array
+  const [repos, setRepos] = useState([])
 
-function App() {
-  async function handleAddRepository() {
-    // TODO
+  // fetch repositories on page load
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepos(response.data)
+    })
+  }, [])
+
+  async function handleAddRepository () {
+    const response = await api.post('repositories', {
+      title: `Desafio Node.js ${Date.now()}`,
+      url: 'https://github.com/...',
+      techs: ['Node.js', '...']
+    })
+    setRepos([...repos, response.data])
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  async function handleRemoveRepository (id) {
+    await api.delete(`repositories/${id}`)
+    const newRepos = repos.filter(repo => {
+      if(repo.id !== id){
+        return repo
+      } else {
+        return false
+      }
+    })
+    setRepos(newRepos)
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+      <ul data-testid='repository-list'>
+        {repos.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
-          </button>
-        </li>
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
